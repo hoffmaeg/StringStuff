695e3b405ea4dfe5527305bcb38d020b700458db
hoffmaeg <hoffmaeg@LCBLAB2110-38.uccob.uc.edu>
2019-12-03 09:16:36 -0500

    in class 12/3

--- StringStuff/src/strings/Main.java
+++ StringStuff/src/strings/Main.java
@@ -14,6 +14,10 @@ public class Main {
 		String test = "Todd Lindemann";
 		long startTime = System.nanoTime();
 		for (int i = 0; i < 1000000000; i++) {
+			// test = "Oscar Robertson";  // initialize a string variable with a string literal
+			// test = test + "!";  // append operation with a string type; creates a new string
+			String fruit = "Applee";// Oops. Spelled wrong!
+			fruit = fruit.replace("ee",  "e");
 
 		}
 		long endTime = System.nanoTime();
@@ -29,7 +33,14 @@ public class Main {
 		StringBuilder test = new StringBuilder("Todd Lindemann");
 		long startTime = System.nanoTime();
 		for (int i = 0; i < 1000000000; i++) {
-
+			// test = new StringBuilder("Oscar Robertson");
+			// test = test + "!"; doesn't work for string builder
+			// test.append("!");
+			test = new StringBuilder("Applee");
+			// System.out.println("Before replace, test = " + test.toString());
+			test.replace(4, 7, "e");  // replace ee with e
+			// System.out.println("After replace, test = " + test.toString());
+			// System.out.println("");
 		}
 		long endTime = System.nanoTime();
 
